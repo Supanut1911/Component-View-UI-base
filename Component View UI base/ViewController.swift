3d009562a99ea0ae3c2f986449bc3724c58cb930
supanut1911 <ynotmela@gmail.com>
2020-05-26 09:37:47 +0700

    Initial Commit

---
+++ Component View UI base/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Component View UI base
+//
+//  Created by Supanut Laddayam on 26/5/2563 BE.
+//  Copyright © 2563 Supanut LDM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

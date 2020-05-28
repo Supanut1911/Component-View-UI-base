52c601b7f6ba0938442886dbe271de2b682c0bba
supanut1911 <ynotmela@gmail.com>
2020-05-28 21:35:50 +0700

    [Add 🅰️] - Scroll View

---
+++ Component View UI base/ScrollView/ScrollViewVC.swift
@@ -0,0 +1,30 @@
+//
+//  ScrollViewVC.swift
+//  Component View UI base
+//
+//  Created by Supanut Laddayam on 26/5/2563 BE.
+//  Copyright © 2563 Supanut LDM. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
